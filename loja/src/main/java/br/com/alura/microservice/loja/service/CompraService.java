c979699ae1d7dcd09fc64134ad6ed08e589221ee
GeovanGoes <geovansilvagoes@gmail.com>
2020-03-28 00:06:41 -0300

    Removendo impl de client com RestTemplate e usando @FeignClient

--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,33 +1,22 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
-public class CompraService {
-
+public class CompraService 
+{
 	@Autowired
-	private RestTemplate client;
-	
-	@Autowired
-	private DiscoveryClient eurekaClient;
-	
+	private FornecedorClient fornecedorClient;
+		
 	public void realizaCompra(CompraDTO compraDto)
 	{		
-		ResponseEntity<InfoFornecedorDTO> response = client.exchange("http://fornecedor/info/" + compraDto.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
-		
-		System.out.println(response.getBody().getEndereco());
-		
-		eurekaClient.getInstances("fornecedor").stream().forEach(fornecedor -> {
-			System.out.println(fornecedor.getUri().toString() + fornecedor.getPort());
-		});
+		InfoFornecedorDTO infoPorEstado = this.fornecedorClient.getInfoPorEstado(compraDto.getEndereco().getEstado());
+		System.out.println(infoPorEstado.getEndereco());
 		
 	}
 }
