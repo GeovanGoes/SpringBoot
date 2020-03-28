d6327911da409d67b5e423d7f087d6401542457d
GeovanGoes <geovansilvagoes@gmail.com>
2020-03-28 19:38:32 -0300

    criando README para os projetos

--- API Rest/Forum/bin/main/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$Lo8Mvw8I8xl4Y/5qVedzwuOg3NBdINjOkjM2WHEscp2t8V0EdddMi');
-
-INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programacao');
-INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
-
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida 2', 'Projeto nao compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
