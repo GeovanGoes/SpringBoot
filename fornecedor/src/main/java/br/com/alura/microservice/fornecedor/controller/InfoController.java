81c243af0d435663ab86865338e8f3b790d14699
GeovanGoes <geovansilvagoes@gmail.com>
2020-03-28 01:12:43 -0300

    papertrail impl, juntar todos os logs em um só

--- fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
+++ fornecedor/src/main/java/br/com/alura/microservice/fornecedor/controller/InfoController.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.fornecedor.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,15 @@ import br.com.alura.microservice.fornecedor.service.InfoService;
 @RequestMapping("/info")
 public class InfoController {
 	
+	
+	private static final Logger _log = LoggerFactory.getLogger(InfoController.class);
+	
 	@Autowired
 	private InfoService infoService;
 	
 	@RequestMapping("/{estado}")
 	public InfoFornecedor getInfoPorEstado(@PathVariable String estado) {
+		_log.info("Recebendo pedido de informaçoes do fornecedor de " + estado);
 		return infoService.getInfoPorEstado(estado);
 	}
 }
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,7 @@
 package br.com.alura.microservice.loja.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +14,30 @@ import br.com.alura.microservice.loja.model.Compra;
 @Service
 public class CompraService 
 {
+	
+	private static final Logger _log = LoggerFactory.getLogger(CompraService.class);
+	
 	@Autowired
 	private FornecedorClient fornecedorClient;
 		
 	public Compra realizaCompra(CompraDTO compraDto)
 	{		
+		_log.info("Indo buscar informacoes do fornecedor... estado: " + compraDto.getEndereco().getEstado());
 		InfoFornecedorDTO infoPorEstado = this.fornecedorClient.getInfoPorEstado(compraDto.getEndereco().getEstado());
-		System.out.println(infoPorEstado.getEndereco());
+		_log.info("INformacoes obtidas.");
 		
+		_log.info(infoPorEstado.getEndereco());
 		
+		_log.info("Indo criar pedido no fornecedor...");
 		InfoPedidoDTO pedidoDTO = fornecedorClient.realizaPedido(compraDto.getItens());
-		
-		System.out.println(pedidoDTO.getTempoDePreparo());
+		_log.info("Pedido realizado no fornecedor.");
+	
 		Compra compra = new Compra();
 		
 		compra.setPedidoId(pedidoDTO.getId());
 		compra.setTempoDePreparo(pedidoDTO.getTempoDePreparo());
 		compra.setEnderecoDestino(compraDto.getEndereco().toString());
-		
+		_log.info("Retornando dados para o usuario");
 		return compra;
 	}
 }
