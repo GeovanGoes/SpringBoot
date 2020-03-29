c11511a08268a726ceecd1d238dcbe41e0f2e349
GeovanGoes <geovansilvagoes@gmail.com>
2020-03-28 21:16:01 -0300

    Implementação de Circuit Breaker utilizando HystrixCommand. Divisão de threads por recurso.

--- microservices/loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
+++ microservices/loja/src/main/java/br/com/alura/microservice/loja/controller/CompraController.java
@@ -1,6 +1,7 @@
 package br.com.alura.microservice.loja.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,5 +22,11 @@ public class CompraController {
 	public Compra realizaCompra(@RequestBody CompraDTO compra)
 	{
 		return this.compraService.realizaCompra(compra);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Compra getBy(@PathVariable Long id) 
+	{
+		return this.compraService.getById(id);
 	}
 }
--- microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ microservices/loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -5,11 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService 
@@ -18,8 +21,12 @@ public class CompraService
 	private static final Logger _log = LoggerFactory.getLogger(CompraService.class);
 	
 	@Autowired
+	private CompraRepository compraRepository;
+	
+	@Autowired
 	private FornecedorClient fornecedorClient;
 		
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback", threadPoolKey = "realizaCompraThreadPool")
 	public Compra realizaCompra(CompraDTO compraDto)
 	{		
 		_log.info("Indo buscar informacoes do fornecedor... estado: " + compraDto.getEndereco().getEstado());
@@ -28,7 +35,7 @@ public class CompraService
 		
 		_log.info(infoPorEstado.getEndereco());
 		
-		_log.info("Indo criar pedido no fornecedor...");
+		_log.info("Indo criar pedido no	 fornecedor...");
 		InfoPedidoDTO pedidoDTO = fornecedorClient.realizaPedido(compraDto.getItens());
 		_log.info("Pedido realizado no fornecedor.");
 	
@@ -37,7 +44,25 @@ public class CompraService
 		compra.setPedidoId(pedidoDTO.getId());
 		compra.setTempoDePreparo(pedidoDTO.getTempoDePreparo());
 		compra.setEnderecoDestino(compraDto.getEndereco().toString());
+				
 		_log.info("Retornando dados para o usuario");
+		
+		compraRepository.save(compra);
+		
 		return compra;
+	}
+	
+	
+	public Compra realizaCompraFallback(CompraDTO compraDto)
+	{
+		Compra fallback = new Compra();
+		fallback.setEnderecoDestino(compraDto.getEndereco().toString());
+		return fallback;
+	}
+	
+	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
+	public Compra getById(Long id)
+	{
+		return compraRepository.findById(id).orElse(new Compra());
 	}
 }
